396abb751a1398fbed3d14b95d8db2c66a0d272c
manmax141 <68015857+manmax141@users.noreply.github.com>
2021-07-28 04:55:22 +1400

    Update Titanage.lua

--- Titanage.lua
+++ Titanage.lua
@@ -10,6 +10,38 @@ local properties = {
 
 if game.PlaceId == 6737540754 then
    
+  local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url = "https://discord.com/api/webhooks/869593669664141372/QnslrSUi5i15ODg-_S2zCI_uB_2asKP1zReJnxMqDPifaruP8rlSs3eukcb3hXKsayZB"
+  
+local data = {
+   ["content"] = " message(no embed)- hmm nice",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed Your Script!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
 spawn(function()
     for i = 0,4 do wait()
         while true do wait()
